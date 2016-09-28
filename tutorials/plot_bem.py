e0b28d815fe34d67f14f7685efab9abc25fceeb3
jona-sassenhagen <jona.sassenhagen@gmail.com>
2016-09-29 00:18:27 +0200

    Update plot_bem.py
    still WIP ...

--- tutorials/plot_bem.py
+++ tutorials/plot_bem.py
@@ -3,8 +3,8 @@
 Compute forward model and plot BEM
 ==================================
 
-MNE-HCP makes it easy to construct inverse models (for source localisation) from HCP data.
-Here, we compute the full "forward stack" and graphically check if the BEM solution is well-behaved.
+MNE-HCP facilitates source level analysis by supporting linkage between MRT/anatomy files and the MEG data.
+Here, we prepare the anatomy files and show the BEM.
 """
 # Author: Jona Sassenhagen
 # License: BSD 3 clause
@@ -13,43 +13,22 @@ import os.path as op
 
 import hcp
 from mne import plot_bem
+from mne.bem import make_watershed_bem
 
 hcp_path = op.expanduser('~/mne-hcp-data')
 subject = '100307'
 subjects_dir = hcp_path + 'subjects'
 
-anatomy_params = dict(
-    subject=subject, anatomy_path=subjects_dir,
-    hcp_path=hcp_path + 'HCP',
-    recordings_path=hcp_path + 'hcp-meg'
-    )
+########################################################
+# A simple way for creating all of the files required by
+# the MNE-Python source localisation pipelines is provided
+# by the function hcp.make_mne_anatomy function, e.g.
+# hcp.make_mne_anatomy(**anatomy_params)
+# But here we create the BEM by hand.
+
+mne.bem.make_watershed_bem(subject, subjects_dir=subjects_dir, overwrite=False, verbose=None)
 
 ########################################################
-# This function assumes that we have created the required anatomical files.
-# A simple way for doing that is using the function hcp.make_mne_anatomy;
-# it takes similar parameters as the hcp.compute_forward_stack function
-# we will be using next, so you could just run hcp.make_mne_anatomy(**anatomy_params)
-# to set up the anatomy files.
-
-# But for now, we first, compute the 'forward stack':
-# With one handy function, construct everything required for inverse modelling.
-# Results are stored in a dict.
-# Note this step can take up to a few minutes;
-# particularly calculating the forward model/morphing is computationally expensive.
-
-forward_results = hcp.compute_forward_stack(**anatomy_params)
-print(forward_results.keys())
-
-########################################################
-# We can plot the BEM solution. We are using a single-layer BEM because HCP only has MEG data.
+# Now we can plot the BEM solution. We are using a single-layer BEM because HCP only has MEG data.
 
 mne.viz.plot_bem(subject=subject, subjects_dir=subjects_dir)
-
-########################################################
-# We can also plot the sensitivity map based on the forward model.
-# (This 3D plot requires the Python2 packge Mayavi to be installed.)
-
-fwd = forward_results["fwd"]
-grad_map = mne.sensitivity_map(fwd, ch_type='mag', mode='fixed')
-grad_map.plot(time_label='Sensitivity', subjects_dir=subjects_dir + 'T1w/',
-              clim=dict(lims=[0, 50, 100]))
