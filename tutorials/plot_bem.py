91e2394d37acd2a3b3fe1b1a404a41451b55765f
jona-sassenhagen <jona.sassenhagen@gmail.com>
2016-09-28 23:37:39 +0200

    add sensitivity map

--- tutorials/plot_bem.py
+++ tutorials/plot_bem.py
@@ -9,10 +9,12 @@ Here, we compute the full "forward stack" and graphically check if the BEM solut
 # Author: Jona Sassenhagen
 # License: BSD 3 clause
 
-import hcp
-import mne
+import os.path as op
 
-hcp_path = '/Volumes/backup/'  # XXX: fix
+import hcp
+from mne import plot_bem
+
+hcp_path = op.expanduser('~/mne-hcp-data')
 subject = '100307'
 subjects_dir = hcp_path + 'subjects'
 
@@ -23,23 +25,31 @@ anatomy_params = dict(
     )
 
 ########################################################
-# First, create the required anatomical files
+# This function assumes that we have created the required anatomical files.
+# A simple way for doing that is using the function hcp.make_mne_anatomy;
+# it takes similar parameters as the hcp.compute_forward_stack function
+# we will be using next, so you could just run hcp.make_mne_anatomy(**anatomy_params)
+# to set up the anatomy files.
 
-hcp.make_mne_anatomy(**anatomy_params)
-
-########################################################
-# Next, compute the 'forward stack':
+# But for now, we first, compute the 'forward stack':
 # With one handy function, construct everything required for inverse modelling.
 # Results are stored in a dict.
 # Note this step can take up to a few minutes;
-# particularly calculating the forward model is computationally expensive.
+# particularly calculating the forward model/morphing is computationally expensive.
 
 forward_results = hcp.compute_forward_stack(**anatomy_params)
 print(forward_results.keys())
 
 ########################################################
-# Now we can plot the BEM solution.
-# We are using a single-layer BEM because HCP only has MEG data.
+# We can plot the BEM solution. We are using a single-layer BEM because HCP only has MEG data.
 
-bem = out["bem_sol"]
 mne.viz.plot_bem(subject=subject, subjects_dir=subjects_dir)
+
+########################################################
+# We can also plot the sensitivity map based on the forward model.
+# (This 3D plot requires the Python2 packge Mayavi to be installed.)
+
+fwd = forward_results["fwd"]
+grad_map = mne.sensitivity_map(fwd, ch_type='mag', mode='fixed')
+grad_map.plot(time_label='Sensitivity', subjects_dir=subjects_dir + 'T1w/',
+              clim=dict(lims=[0, 50, 100]))
