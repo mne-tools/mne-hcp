7532d3966b296f90c7597e30b310c68d0381df8c
Eric Larson <larson.eric.d@gmail.com>
2016-10-01 18:06:56 +0900

    Update __init__.py

--- hcp/__init__.py
+++ hcp/__init__.py
@@ -1,7 +1,7 @@
 # Author: Denis A. Engemann <denis.engemann@gmail.com>
 # License: BSD (3-clause)
 
-__version__ = '0.1.dev6'
+__version__ = '0.1.dev7'
 from . import io
 from . import viz
 from . import preprocessing
